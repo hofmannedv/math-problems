fccaaf160fb91675381b0d001932768c8ce4015c
Frank Hofmann <frank.hofmann@efho.de>
2021-01-07 04:29:46 +0100

    optimized version: weight

--- knapsack/knapsack-opt-weights.py
+++ knapsack/knapsack-opt-weights.py
@@ -2,6 +2,11 @@
 # solution for the Knapsack problem:
 # https://en.wikipedia.org/wiki/Knapsack_problem
 #
+# optimization:
+# ignore the numbers whose value is greater than the maximum capacity of
+# the bag
+# reduces the execution time by up to 25%
+#
 # (C) 2021 Frank Hofmann <frank.hofmann@efho.de>
 # Released under GNU Public License 2.0
 
@@ -15,8 +20,18 @@ numbers = [5, 12, 3, 4, 26, 8, 13, 1, 7, 11, 33, 10]
 
 # print initial status
 numberOfItems = len(numbers)
-print("numbers: ", numbers)
+print("original numbers: ", numbers)
 print("calculating the possible combinations: for a maximum capacity of %i" % maximumCapacity)
+
+# remove the numbers that do not fit into the bag
+adjustedNumbers = []
+for entry in numbers:
+    if (entry <= maximumCapacity):
+        adjustedNumbers.append(entry)
+
+# print corrected number set
+numberOfItems = len(adjustedNumbers)
+print("adjusted number set: ", adjustedNumbers)
 
 # try to find one of the best solutions
 # result will show just one, also if more than one exist
@@ -26,7 +41,7 @@ bestSolutionItems = ()
 # calculate possible combinations of numbers
 for runs in range(0, numberOfItems + 1):
     # calculate the combinations for the selected numbers
-    for subset in itertools.combinations(numbers, runs):
+    for subset in itertools.combinations(adjustedNumbers, runs):
         # define intermediate total
         temporarySum = 0
 
